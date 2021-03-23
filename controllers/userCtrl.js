88f85349c50222d74701b4c7a4bfae28b083e1a0
ahmed-bouhrira <ahmedbouhrira365@gmail.com>
2021-03-23 01:27:57 +0100

    infos user

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -82,6 +82,15 @@ const userCtrl = {
     } catch (err) {
       return res.status(500).json({ message: err.message });
     }
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id).select(-password);
+      if (!user) return res.status(400).json({ message: "user not found" });
+      res.json({ user });
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
   }
 };
 const createAccessToken = (user) => {
